3545274658cbdf0f586e2e95b571878f60e7c5d5
FrankZhaopj <399109168@qq.com>
2017-09-23 01:34:01 +0800

    add jackson 2.8.1

--- myspring/src/main/java/com/zhaopj/spring/web/TestController.java
+++ myspring/src/main/java/com/zhaopj/spring/web/TestController.java
@@ -1,7 +1,13 @@
 package com.zhaopj.spring.web;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 @RequestMapping("/test")
@@ -11,6 +17,16 @@ public class TestController {
 	public String show() {
 		System.out.println("myspring is ready ...");
 		return "test/test";
+	}
+	
+	@RequestMapping("/json")
+	@ResponseBody
+	public Map<String, Object> json() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("username", "zaopj");
+		map.put("password", "123456");
+		map.put("data", new Date());
+		return map;
 	}
 	
 }
