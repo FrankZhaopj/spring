bf191da76f16b9b01d81952748be074621b7793b
FrankZhaopj <399109168@qq.com>
2017-09-22 23:13:26 +0800

    myspring

---
+++ myspring/src/main/java/com/zhaopj/spring/web/MyTest.java
@@ -0,0 +1,16 @@
+package com.zhaopj.spring.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class MyTest {
+
+	@RequestMapping("/show")
+	public String show() {
+		System.out.println("myspring is ready ...");
+		return "test/test";
+	}
+	
+}
---
+++ myspring/src/main/webapp/WEB-INF/views/test/test.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>my  spring mvc fist page ... </h2>
+</body>
+</html>
